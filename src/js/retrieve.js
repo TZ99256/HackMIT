8ea9a850352fd3e390fed55233ec88d5b161edf8
mitadm <mitadm@C02ND15BG5RP.tld>
2019-09-15 04:55:13 -0400

    visualization is BEAUTIFUL

--- src/js/retrieve.js
+++ src/js/retrieve.js
@@ -1,4 +1,4 @@
-/*var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDXDJJov4KMkWkK__CQ-JAckT_0Kk69lLA",
     authDomain: "hackmit-recon.firebaseapp.com",
     databaseURL: "https://hackmit-recon.firebaseio.com",
@@ -22,6 +22,8 @@ var result = retrieve.on('value',gotData,errData);
 
 
 firebase.database().ref('Items').once('value').then(function(snapshot) {
+    var props = {'food': 0.0, 'clothing': 0.0, 'furniture': 0.0, 'transportation': 0.0, 'health': 0.0, 'leisure': 0.0, 'other': 0.0};
+    var total = 0.0;
     snapshot.forEach(function(userSnapshot) {
         var things = []
         var username = userSnapshot.val();
@@ -43,22 +45,40 @@ firebase.database().ref('Items').once('value').then(function(snapshot) {
                 preOb.appendChild(box)
             }
         }
+
+        props[things[0]["category"]] += parseFloat(things[0]['price']);
+        total += parseFloat(things[0]['price']);
+        
     });
+    for(var key in props){
+        props[key] = Math.round(props[key]/total * 100);
+    }
+    
+    var pics = {'food': '<img class = "emoji" src= "../images/food.png"/>',
+            'clothing': '<img class = "emoji" src = "../images/clothes.png"/>',
+            'furniture': '<img class = "emoji" src = "../images/home.png"/>',
+            'transportation': '<img class = "emoji" src = "../images/transportation.png"/>',
+            'health': '<img class = "emoji" src = "../images/health.png"/>',
+            'leisure': '<img class = "emoji" src = "../images/leisure.png"/>',
+            'other': '<img class = "emoji" src = "../images/think.png"/>'}
+
+    $(function ()
+    {
+        $('#update').on('click', function ()
+        {
+            for(var key in props){
+                for (i=0; i<props[key]; i++){
+                    var key2 = ('#').concat(key);
+                    $(key2).prepend(pics[key]);
+                }
+            }
+        });
+
+    });
+
+
 });
 
-
-<<<<<<< HEAD
-  
-var preOb = document.getElementById('object');
-for (var key in things){
-    var box = document.createElement('div');
-    box.innerText = things[key]
-    preOb.appendChild(box)
-}
-=======
-
-
->>>>>>> f1411435fc759928bdd91099921dbf5ce819758c
 
 function gotData(data){
     console.log(data.val()); //this is where the data is
@@ -69,39 +89,4 @@ function errData(err) {
     console.log(err)
 }
 
-console.log(things);
 
-var props = {'food': 0, 'clothing': 0, 'furniture': 0, 'transportation': 0, 'health': 0, 'leisure': 0, 'other': 0};
-var total = 0;
-for(i=0; i<things.length; i++){
-    var attrs = things[i];
-    console.log(attrs);
-    props[attrs["category"]] += attrs['value'];
-    total += attrs['value'];
-}
-console.log(total);
-for(var key in props){
-    props[key] = Math.round(props[key]/total * 100);
-}
-console.log(props);
-var pics = {'food': '<img class = "emoji" src= "../images/food.png"/>',
-            'clothing': '<img class = "emoji" src = "../images/clothes.png"/>',
-            'furniture': '<img class = "emoji" src = "../images/home.png"/>',
-            'transportation': '<img class = "emoji" src = "../images/transportation.png"/>',
-            'health': '<img class = "emoji" src = "../images/health.png"/>',
-            'leisure': '<img class = "emoji" src = "../images/leisure.png"/>',
-            'other': '<img class = "emoji" src = "../images/misc.jpeg"/>'}
-
-$(function ()
-{
-    $('#update').on('click', function ()
-    {
-        for(var key in props){
-            for (i=0; i<props[key]; i++){
-                var key2 = ('#').concat(key);
-                $(key2).prepend(pics[key]);
-            }
-        }
-    });
-
-});*/
