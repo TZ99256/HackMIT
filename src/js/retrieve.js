df453d36edc44d5e8698920322552d54166645a9
Umarbek Nasimov <unasimov1256@bths.edu>
2019-09-14 21:42:24 -0400

    fixed version

--- src/js/retrieve.js
+++ src/js/retrieve.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDXDJJov4KMkWkK__CQ-JAckT_0Kk69lLA",
+    authDomain: "hackmit-recon.firebaseapp.com",
+    databaseURL: "https://hackmit-recon.firebaseio.com",
+    projectId: "hackmit-recon",
+    storageBucket: "hackmit-recon.appspot.com",
+    messagingSenderId: "357844945838",
+    appId: "1:357844945838:web:dc443973b9dd335a40963f"
+};
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+var database = firebase.database();
 var retrieve = database.ref('Items');
 /*
 starCountRef.on('value', function(snapshot) {
@@ -12,10 +25,7 @@ function gotData(data){
     console.log(data.val()); //this is where the data is
     var x = data.val()
 }
-console.log(x)
 function errData(err) {
     console.log('Error!');
     console.log(err)
 }
-
-export default x;
