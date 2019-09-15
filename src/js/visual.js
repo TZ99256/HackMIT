d2d0630ea874bfdace99dc8b959fb854cccb27d9
mitadm <mitadm@C02ND15BG5RP.tld>
2019-09-14 22:01:36 -0400

    visuals

--- src/js/visual.js
+++ src/js/visual.js
@@ -1,16 +1,27 @@
+var data = {1: {'ID': '#food', 'Purchase Category': 'Dining', 'Price': 12.05},
+            2: {'ID': '#food', 'Purchase Category': 'Dining', 'Price': 3.12},
+            3: {'ID': '#transportation', 'Purchase Category': 'Transportation', 'Price': 4.1}};
+
+var props = {'#food': 0, '#utilities': 0, '#clothing': 0, '#transportation': 0, '#home': 0, '#misc': 0};
+var total = 0;
+for(var key in data.keys()){
+    props[data[key]['ID']] += 1;
+    total += 1;
+}
+for(var key in props.keys()){
+    props[key] = Math.round(props[key]/total);
+}
 
 $(function ()
 {
-    $('#food').on('click', function ()
+    $('#update').on('click', function ()
     {
-        $(this).width($(this).width()*0.1+$(this).width());
-        $(this).height($(this).height()*0.1+$(this).height());
+        for(var key in props.keys()){
+            for (i=0; i<props[key]; i++){
+                $(key)
+            }
+        }
     });
 
-    $('#utilities').on('click', function()
-    {
-        $(this).width($(this).width()*0.1+$(this).width());
-        $(this).height($(this).height()*0.1+$(this).height());
-    })
 });
 
