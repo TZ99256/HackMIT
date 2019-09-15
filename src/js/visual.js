651216ec5aead4e7a2b7e4ffe684e6f64d846105
mitadm <mitadm@C02ND15BG5RP.tld>
2019-09-14 23:28:23 -0400

    added a visualization

--- src/js/visual.js
+++ src/js/visual.js
@@ -4,21 +4,28 @@ var data = {1: {'ID': '#food', 'Purchase Category': 'Dining', 'Price': 12.05},
 
 var props = {'#food': 0, '#utilities': 0, '#clothing': 0, '#transportation': 0, '#home': 0, '#misc': 0};
 var total = 0;
-for(var key in data.keys()){
-    props[data[key]['ID']] += 1;
-    total += 1;
+for(var key in data){
+    var attrs = data[key];
+    props[attrs["ID"]] += attrs['Price'];
+    total += attrs['Price'];
 }
-for(var key in props.keys()){
-    props[key] = Math.round(props[key]/total);
+for(var key in props){
+    props[key] = Math.round(props[key]/total * 100);
 }
+var pics = {'#food': '<img class = "emoji" src= "../images/food.png"/>',
+            '#utilities': '<img class = "emoji" src = "../images/utilities.jpg"/>',
+            '#clothing': '<img class = "emoji" src = "../images/clothes.png"/>',
+            '#transportation': '<img class = "emoji" src = "../images/transportation.png"/>',
+            '#home': '<img class = "emoji" src = "../images/home.png"/>',
+            '#misc': '<img class = "emoji" src = "../images/misc.jpeg"/>'}
 
 $(function ()
 {
     $('#update').on('click', function ()
     {
-        for(var key in props.keys()){
+        for(var key in props){
             for (i=0; i<props[key]; i++){
-                $(key).prepend('<img src= "../../images/food.png" />')
+                $(key).prepend(pics[key]);
             }
         }
     });
