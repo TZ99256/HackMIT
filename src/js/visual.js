8a6457a5d2f0c0c4b4f3717b4a8b5b0709a35725
Sophia Zhi <sophiazhi@gmail.com>
2019-09-14 21:59:37 -0400

    emojis but they might not work

--- src/js/visual.js
+++ src/js/visual.js
@@ -14,3 +14,22 @@ $(function ()
     })
 });
 
+function emojify(item, category) {
+    const emoji = require('node-emoji');
+    const category_emojis = {
+        'dining': emoji.find('knife_fork_plate'),
+        'transportation': emoji.find('car'),
+        'groceries': emoji.find('shopping_trolley'),
+        'clothing': emoji.find("shirt"),
+        'utilities': emoji.find("shower"),
+        'other': emoji.find("thinking_face"),
+    };
+
+    if (emoji.hasEmoji(item)) {
+        return emoji.find(item).emoji;
+    } else if (emoji.hasEmoji(category)) {
+        return emoji.find(category).emoji;
+    } else {
+        return emoji.find("thinking_face");
+    }
+}
