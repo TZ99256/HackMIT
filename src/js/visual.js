59438eadcfd518af7be4566f388259195dbb4094
mitadm <mitadm@C02ND15BG5RP.tld>
2019-09-14 21:05:12 -0400

    bigger

--- src/js/visual.js
+++ src/js/visual.js
@@ -1,5 +1,16 @@
-function higher(){
-    var foodbutton = document.getElementById("food")
-    var currh = foodbutton.style.height;
-    foodbutton.style.height = 5*currh;
-}
+
+$(function ()
+{
+    $('#food').on('click', function ()
+    {
+        $(this).width($(this).width()*0.1+$(this).width());
+        $(this).height($(this).height()*0.1+$(this).height());
+    });
+
+    $('#utilities').on('click', function()
+    {
+        $(this).width($(this).width()*0.1+$(this).width());
+        $(this).height($(this).height()*0.1+$(this).height());
+    })
+});
+
